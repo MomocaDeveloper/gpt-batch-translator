018badcb9111b0dec6f0da22c582bb5adcf58a1b
Atlas.Wing <411328563@qq.com>
2023-03-29 18:28:01 +0800

    Update translate.py
    Propose parameters and optimize comments.

--- translateClient/translate.py
+++ translateClient/translate.py
@@ -5,27 +5,28 @@ import time
 import json
 import math
 
+httpUrl = 'http://{your_IP}:{your_port}/'
 
 def upload_file():
     file_list = os.listdir("./upload")
     for i, file_name in enumerate(file_list):
         print(f"{i+1}. {file_name}")
-    selected_file_index = int(input("请输入要上传的文件编号："))
+    selected_file_index = int(input("Please enter the file number to upload："))
     fileName = str(file_list[selected_file_index - 1])
     selected_file_name = "./upload/" + str(file_list[selected_file_index - 1])
     
-    print("正在上传文件:", selected_file_name)  
+    print("uoloading:", selected_file_name)  
     
-    upload_url = 'http://35.219.174.249:9003/upload'
+    upload_url = httpUrl + 'upload'
     files = {'file': open(selected_file_name, 'rb')}
-    # 发起Post请求上传文件
-    print("发起POST请求:", selected_file_name)  
+    # Post update file
+    print("start POST:", selected_file_name)  
     response = requests.post(upload_url, files=files)
     return fileName
     
 #fileName="test1.xlsx"
 def download_file(fileName):
-    download_url = 'http://35.219.174.249:9003/download?type=download&file='
+    download_url = httpUrl + 'download?type=download&file='
     download_url = download_url + 'tr_' + fileName
     response = requests.get(download_url)
     
@@ -33,14 +34,14 @@ def download_file(fileName):
         f.write(response.content)
 
 def translate_file(fileName):
-    translate_url = 'http://35.219.174.249:9003/translate?temperature=0.7&file='
-    #translate_url = 'http://35.219.174.249:9003/translate?temperature=0&file='
+    # can edit your temperature here
+    translate_url = httpUrl + 'translate?temperature=0.7&file='
     translate_url = translate_url + fileName
     response = requests.get(translate_url)
     #print(response.content)
     
 def progress_file(fileRealName, result):
-    progress_url = 'http://35.219.174.249:9003/progress'
+    progress_url = httpUrl + 'progress'
     response = requests.get(progress_url)
     try:
         resp_content = response.json()
@@ -62,9 +63,9 @@ def timer_api(fileName):
     _result = 0
     _lastProgress = 0
     fileRealName = fileName.split('.')[0]
-    fanyiText = "翻译进度:"
+    fanyiText = "translate progress:"
     badTokenCount = 0
-    print("翻译中，请等待，当前" + fanyiText + "0%")
+    print("translating，wait please，now " + fanyiText + "0%")
     while (_result <= 99 and index < 10000):
         _result, errcode = progress_file(fileRealName, _result)
         if errcode == 999:
@@ -84,14 +85,13 @@ def timer_api(fileName):
     
 if __name__ == '__main__':
     fileName = upload_file()
-    print("上传文件完成,开始翻译文件:", fileName)
+    print("uoload sucess, start translate:", fileName)
     translate_file(fileName)
-    print("显示翻译文件进度:", fileName)
     start = time.time()
     timer_api(fileName)
     end = time.time()
     time.sleep(6)
     runtime = math.floor(end - start)
-    print("翻译进度:100%")
-    print("翻译总时长:", str(runtime) + "秒")
+    print("translate progress:100%")
+    print("Translation total duration:", str(runtime) + "sec")
     download_file(fileName)
