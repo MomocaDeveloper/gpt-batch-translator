79ba47f9be186eb8eb61ca99380fa8087cd5aedc
Atlas.Wing <411328563@qq.com>
2023-03-29 18:29:24 +0800

    Update uploadRole.py

--- translateClient/uploadRole.py
+++ translateClient/uploadRole.py
@@ -5,13 +5,13 @@ import time
 import json
 import sys
 
-
+httpUrl = 'http://{your_IP}:{your_port}/'
 def upload_file(fileName):
     #selected_file_name = "./" + fileName
  
     print("正在上传文件:", fileName)  
     myobj = {'type': 3}
-    upload_url = 'http://35.219.174.249:9003/upload'
+    upload_url = httpUrl + 'upload'
     files = {'file': open(fileName, 'rb')}
     # 发起Post请求上传文件
     print("发起POST请求:", fileName)  
